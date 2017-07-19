a95dbd83f2eda76fb408cbb25ff46fc69d82ce8a
spookyGitHub <contact@spooklight.ch>
2017-07-19 14:10:57 +0200

    Creating HexCell and HexGrid. Next step : Access coordinate

---
+++ HexMap/Assets/Scripts/HexMap/HexGrid.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class HexGrid : MonoBehaviour {
+
+	#region Variables
+
+	[SerializeField]
+	int _width = 6;
+
+	[SerializeField]
+	int _height = 6;
+
+	[SerializeField]
+	HexCell _cellPrefab;
+
+	public int Width {
+		get { return _width; }
+	}
+
+	public int Height {
+		get { return _height; }
+	}
+
+	HexCell[] _cells;
+
+	#endregion
+
+	#region Unity_Methods
+
+	void Awake() {
+		_cells = new HexCell[_height * _width];
+
+		for (int z = 0, i = 0; z < _height; ++z) {
+			for (int x = 0; x < _width; ++x) {
+				CreateCell (x, z, i++);
+			}
+		}
+	}
+
+	#endregion
+
+	#region Methods
+
+	void CreateCell(int p_x, int p_z, int p_i) {
+		Vector3 position;
+		position.x = p_x * 10f;
+		position.y = 0f;
+		position.z = p_z * 10f;
+
+		HexCell Cell = _cells[p_i] = Instantiate<HexCell> (_cellPrefab);
+		Cell.transform.SetParent (transform, false);
+		Cell.transform.localPosition = position;
+
+	}
+
+	#endregion
+}
---
+++ HexMap/Assets/Scripts/Parameters/HexMetrics.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class HexMetrics {
+	public const float _outerRadius = 10f;
+	public const float _innerRadius = _outerRadius * 0.866025404f;
+
+	public static Vector3[] _corners = {
+		new Vector3 (0f, 0f, _outerRadius),
+		new Vector3 (_innerRadius, 0f, 0.5f * _outerRadius),
+		new Vector3 (_innerRadius, 0f, -0.5f * _outerRadius),
+		new Vector3 (0f, 0f, -_outerRadius),
+		new Vector3 (-_innerRadius, 0f, -0.5f * _outerRadius),
+		new Vector3 (-_innerRadius, 0f, 0.5f * _outerRadius)
+	};
+}
