7219a63b8565c2387858ec303804757b0f9e77ba
spookyGitHub <contact@spooklight.ch>
2017-08-15 08:50:24 +0200

    Next step: Moving the camera

--- HexMap/Assets/Scripts/Input/HexMapCamera.cs
+++ HexMap/Assets/Scripts/Input/HexMapCamera.cs
@@ -4,13 +4,53 @@ using UnityEngine;
 
 public class HexMapCamera : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	#region Variables
+
+	[SerializeField]
+	float _stickMinZoom;
+
+	[SerializeField]
+	float _stickMaxZoom;
+
+	[SerializeField]
+	float _swivelMinZoom;
+
+	[SerializeField]
+	float _swivelMaxZoom;
+
+	Transform _swivel;
+	Transform _stick;
+	float _zoom = 1f;
+
+	#endregion
+
+	#region Unity_methods
+
+	void Awake() {
+		_swivel = transform.GetChild (0);
+		_stick = _swivel.GetChild (0);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update() {
+		float zoomDelta = Input.GetAxis ("Mouse ScrollWheel");
+		if (zoomDelta != 0f) {
+			AdjustZoom (zoomDelta);
+		}
 	}
+
+	#endregion
+
+	#region Methods
+
+	void AdjustZoom(float p_zoomDelta) {
+		_zoom = Mathf.Clamp01 (_zoom + p_zoomDelta);
+
+		float distance = Mathf.Lerp (_stickMinZoom, _stickMaxZoom, _zoom);
+		_stick.localPosition = new Vector3 (0f, 0f, distance);
+
+		float angle = Mathf.Lerp(_swivelMinZoom, _swivelMaxZoom, _zoom);
+		_swivel.localRotation = Quaternion.Euler (angle, 0f, 0f);
+	}
+
+	#endregion
 }
